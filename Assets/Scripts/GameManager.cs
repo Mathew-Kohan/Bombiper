90b5a4aa3947c0eccf8ee138b081303bdfe59004
Matt Kohan <mh.kl18@yahoo.com>
2022-12-11 23:24:56 +0330

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int dieNum;
+    
+    public GameObject[] lights;
+
+    [SerializeField]
+    private float moveTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(Activelights());
+        }
+
+    }
+
+
+    private IEnumerator Activelights()
+    {
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (i < dieNum)
+            {
+                lights[i].SetActive(true);
+            }
+            else
+            {
+                lights[i].SetActive(false);
+            }
+
+            if (dieNum == 4)
+            {
+                dieNum = 7;
+            }
+            yield return new WaitForSeconds(moveTime);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed;
+
+
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.left, verticalInput * _rotationSpeed * Time.deltaTime);
+    }
+}
