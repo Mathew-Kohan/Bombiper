8c955ef8e639a51c1f0fb66a4ac53d047f868c9c
Matt Kohan <mh.kl18@yahoo.com>
2022-12-12 11:19:19 +0330

    Feat add arrow buttons

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -6,12 +6,30 @@ public class RotateCamera : MonoBehaviour
 {
     [SerializeField]
     private float _rotationSpeed;
+    private float _verticalInput;
 
+    [SerializeField]
+    private bool _isRotate = false;
 
     void Update()
     {
-        float verticalInput = Input.GetAxis("Vertical");
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            _verticalInput = Input.GetAxis("Vertical");
+            _isRotate = true;  
+        }
+        if (_isRotate)
+            transform.Rotate(Vector3.left, _verticalInput * _rotationSpeed * Time.deltaTime);
+    }
 
-        transform.Rotate(Vector3.left, verticalInput * _rotationSpeed * Time.deltaTime);
+    public void RotateUp(bool b)
+    {
+        _verticalInput = 1;
+        _isRotate = b;
+    }
+    public void RotateDown(bool b)
+    {
+        _verticalInput = -1;
+        _isRotate = b;
     }
 }
